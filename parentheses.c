24258295bcd4348081f355c8dcb55534fb1c2fc9
Jay Van Dam <vandajay@mail.gvsu.edu>
2020-03-30 12:06:51 -0400

    Getting started here

--- parentheses.c
+++ parentheses.c
@@ -1,1 +1,14 @@
-hello worlds
+#include<stdio.h>
+#include<stdlib.h>
+
+// prototypes
+
+int main() {
+    char valid[] = "(djfk[kfdsj{012}hello]world)";
+    char invalid[] = "(hello[how{are]youtoday})";
+
+    printf("%s\n", valid);
+    printf("%s\n", invalid);
+}
+
+// functions
