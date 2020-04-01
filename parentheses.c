92099d2444b3e3894c3b6c5e29aaefdd09aeeaa3
Jay Van Dam <vandajay@mail.gvsu.edu>
2020-04-01 15:54:03 -0400

    Before first submission

--- parentheses.c
+++ parentheses.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 int isParenthesisValid(char *s, int size);
 
@@ -17,15 +16,15 @@ int main (void) {
        if (ssize == -1)
            return 0;
        else if (isParenthesisValid(sptr, ssize) == 1)
-           printf("VALID\n\n");
+           printf("VALID");
        else
-           printf("NOT VALID\n\n");
+           printf("NOT VALID");
     }
+   free(sptr);
     return 0;
 }
 
 int isParenthesisValid(char *s, int size) {
-    printf("string size = %d\n", size);
     char *arr;
     arr = (char *) malloc(sizeof(char)*size);
     int cnt = 0;
@@ -33,42 +32,33 @@ int isParenthesisValid(char *s, int size) {
 
     for (i = 0; i < size; i++) {
         if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-            printf("-> arr[%d] = %c\n", i, s[i]);
             arr[cnt] = s[i];
             cnt++;
         }
         if(s[i] == ')' || s[i] == ']' || s[i] == '}') {
             if (cnt == 0) {
-                printf("1 length of arr %d\n", (int) strlen(arr));
-                printf("arr = %s\n", arr);
-                printf("s[%d] = %c\n", i, s[i]);
                 return 0;
             }
             else {
                 if (arr[cnt-1] == '(' && s[i] == ')') {
-                    printf("a -> s[%d] = %c\n", i, s[i]);
                     cnt--;
                 }
                 else if (arr[cnt-1] == '[' && s[i] == ']') {
-                    printf("b -> s[%d] = %c\n", i, s[i]);
                     cnt--;
                 }
                 else if (arr[cnt-1] == '{' && s[i] == '}') {
-                    printf("c -> s[%d] = %c\n", i, s[i]);
                     cnt--;
                 }
                 else {
-                    printf("2 length of arr %d\n", (int) strlen(arr));
-                    printf("arr[%d] = %c\n", cnt, arr[cnt]);
-                    printf("s[%d] = %c\n", i, s[i]);
                     return 0;
-                    }
+                }
             }
         }
     }
-    arr[cnt] = '\0';
-    printf("3 length of arr %d\n", (int) strlen(arr));
-    printf("arr = %s\n", arr);
-    printf("s[%d] = %c\n", i, s[i]);
-    return 1;
+    free(arr);
+    if (cnt > 0) {
+        return 0;
+    }
+    else
+        return 1;
 }
