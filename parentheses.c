ebbd51109119789703bf0cf1454a019139cea575
Jay Van Dam <vandajay@mail.gvsu.edu>
2020-04-01 10:26:50 -0400

    Each user string is analized on the fly, still need to work out kinks in validation function

--- parentheses.c
+++ parentheses.c
@@ -2,36 +2,58 @@
 #include<stdlib.h>
 #include<string.h>
 
-int isParenthesisValid(char *p);
+int isParenthesisValid(char *s, int size);
 
 int main (void) {
 
-    char *sptr[10];
-    int cnt = 0;
-    int res = 1+EOF;
+    char *sptr;
+    size_t maxlen = 1000;
+    int ssize = 0;
 
-   while (res != EOF) { 
-        char input[30];
-        sptr[cnt] = (char *) malloc(sizeof(char)*30);
-        res = scanf("%s", input);
-        strcpy(sptr[cnt], input);
-        cnt++;
-    }
-
-//    for(int j = 0; j < cnt-1; j++) {
-//        printf("%s", sptr[j]);
-//    }
-
-
-    for(int j=0; j < cnt-1; j++) {
-        if (isParenthesisValid(*(sptr+j)) == 0)
-            printf("VALID");
+   while (ssize != -1) {
+       sptr = (char *) malloc(sizeof(char)*maxlen);
+       ssize = getline(&sptr, &maxlen, stdin);
+       
+        if (isParenthesisValid(sptr, ssize) == 1)
+            printf("VALID\n");
         else
-            printf("NOT VALID");
+            printf("NOT VALID\n");
     }
     return 0;
 }
 
-int isParenthesisValid(char *p) {
-    return 0;
+int isParenthesisValid(char *s, int size) {
+    printf("string size = %d\n", size);
+    char *arr;
+    arr = (char *) malloc(sizeof(char)*size);
+    int cnt = 0;
+    arr[cnt] = '\0';
+
+    for (int i = 0; i < size; i++) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            printf("%c\n", s[i]);
+            arr[cnt] = s[i];
+            cnt++;
+        }
+        if(s[i] == ')' || s[i] == ']' || s[i] == '}') {
+            if (strlen(arr) == '\0') {
+                printf("1 length of arr %d\n", (int) strlen(arr));
+                return 0;
+            }
+            else {
+                if (arr[cnt] == '(' && s[i] == ')')
+                    cnt--;
+                else if (arr[cnt] == '[' && s[i] == ']')
+                    cnt--;
+                else if (arr[cnt] == '{' && s[i] == '}')
+                    cnt--;
+                else {
+                    printf("2 length of arr %d\n", (int) strlen(arr));
+                    return 0;
+                    }
+            }
+        }
+    }
+    printf("3 length of arr %d\n", (int) strlen(arr));
+    return 1;
 }
